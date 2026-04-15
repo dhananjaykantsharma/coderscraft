b17f31ff19d5d5ca58e0243930da51fd80562c7a
Dhananjay kant sharma <dhananjaykants@gmail.com>
2026-04-15 23:25:33 +0530

    fix the prisma config file

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
-  migrate: {
-    async adapter() {
-      const { PrismaPg } = await import('@prisma/adapter-pg')
-      return new PrismaPg({ connectionString: process.env.DATABASE_URL! })
-    }
-  }
-})
+  datasource: {
+    url: process.env.DATABASE_URL!,
+  },
+});
